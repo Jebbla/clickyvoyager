ea4aa98604a154177b377cf446bfb75aa39e4fff
Jebbla <jlp2722@gmail.com>
2019-09-25 21:08:24 -0500

    Updates

---
+++ precache-manifest.a99dfe41a82fd29232ca6408e20893c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bbf98dc6f69f6cef1315268012118eb",
+    "url": "/clickyvoyager/index.html"
+  },
+  {
+    "revision": "dd3e6c2059277cc42e1b",
+    "url": "/clickyvoyager/static/css/main.88a849c5.chunk.css"
+  },
+  {
+    "revision": "61d8fad47930d8754f7c",
+    "url": "/clickyvoyager/static/js/2.738e08bd.chunk.js"
+  },
+  {
+    "revision": "dd3e6c2059277cc42e1b",
+    "url": "/clickyvoyager/static/js/main.00f265ac.chunk.js"
+  },
+  {
+    "revision": "86104ae9cee6a57a9863",
+    "url": "/clickyvoyager/static/js/runtime-main.24877f68.js"
+  }
+]);
